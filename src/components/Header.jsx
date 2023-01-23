59fe1c10e267c90dcd8654556a5549ac1e8868b3
FacundoSosa <facundososa676@gmail.com>
2023-01-23 17:48:59 -0300

    fix nav mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 import { Link } from 'react-router-dom'
+import menuIcon from '../assets/menu-icon.svg'
 
 function Header() {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(!open)
+  }
+
   return (
-    <header className='header-container pt-3 px-3 sticky-top'>
-        <h1 className='m-0'><Link to='/' className='nav-link fs-1 fw-normal'>FACU SOSA</Link></h1>
-        <NavBar/>
+    <header className='header-container py-2 px-3 sticky-top'>
+      <h1 className='w-min-content m-0'><Link to='/' className='nav-link fs-1 fw-normal'>FACU SOSA</Link></h1>
+      <button onClick={handleOpen} className='btn border-0 p-0 w-min-content'><img src={menuIcon} alt="menu-icon.svg" /></button>
+      <nav className='nav-container text-end'>
+        {open === true && <NavBar />}
+      </nav>
     </header>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,13 @@
-import React, { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import menuIcon from '../assets/menu-icon.svg'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
-  const location = useLocation();
-
-  const [open, setOpen] = useState(false)
-
-  const handleOpen = () => {
-    setOpen(!open)
-  }
-
   return (
-    <nav className='text-end'>
-      <button onClick={handleOpen} className='btn border-0 p-0'><img src={menuIcon} alt="menu-icon.svg" /></button>
-      <div className='line position-absolute end-0'></div>
-      {open === true &&
-      <TransitionGroup>
-        <CSSTransition key={location.pathname} className='position-static' classNames="fade" timeout={1000}>
-          <div className='w-100 nav-links-enter' location={location}>
-            <ul className='d-grid m-0 px-0 py-1'>
-              <li className='list-group-item'><Link to='/about' className='nav-link fs-5 fw-light'>About</Link></li>
-              <li className='list-group-item'><Link to='/projects' className='nav-link fs-5 fw-light'>Projects</Link></li>
-              <li className='list-group-item'><Link to='/contact' className='nav-link fs-5 fw-light'>Contact</Link></li>
-            </ul>
-            <div className='line position-absolute end-0'></div>
-          </div>
-        </CSSTransition>
-      </TransitionGroup>}
-     </nav>
+    <ul className='nav-links d-grid m-0 px-0 py-1'>
+      <li className='nav-links list-group-item my-1'><Link to='/about' className='nav-link fs-6 fw-light'>ABOUT</Link></li>
+      <li className='nav-links list-group-item my-1'><Link to='/projects' className='nav-link fs-6 fw-light'>PROJECTS</Link></li>
+      <li className='nav-links list-group-item my-1'><Link to='/contact' className='nav-link fs-6 fw-light'>CONTACT</Link></li>
+    </ul>
   )
 }
 
