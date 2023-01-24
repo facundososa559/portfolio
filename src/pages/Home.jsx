a5f15075107e481341e84a469f190116abeeb511
FacundoSosa <facundososa676@gmail.com>
2023-01-23 23:10:29 -0300

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
       <div className='portada my-4 mx-1 mb-5'></div>
       <div className='container r w-100 justify-content-evenly'>
         <div className='row'>
-          <h1 className='home-titles text-end col-sm-6'>FRONTEND DEVELOPER.</h1>
-          <h1 className='home-titles text-end col-sm-6'>CLOUD PRACTITIONER.</h1>
+          <h1 className='home-titles text-end col-md-6'>FRONTEND DEVELOPER.</h1>
+          <h1 className='home-titles text-end col-md-6'>CLOUD PRACTITIONER.</h1>
         </div>
       </div>
     </div>
