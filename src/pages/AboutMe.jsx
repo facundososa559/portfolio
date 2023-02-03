e801265008c4bbc1566f096b7843f56f0e99c524
FacundoSosa <facundososa676@gmail.com>
2023-02-03 20:10:42 -0300

    improve about

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,7 +10,7 @@ function AboutMe() {
         My experience includes working with JavaScript and React, and I recently graduated with high marks in Frontend React Development. Additionally, I have obtained my AWS Cloud Practitioner certification and completed the AWS re/Start training program.
       </p>
       <p className="about-paragraph">
-        My goal is to assist companies in improving their online presence, specializing in Frontend technologies and AWS Cloud, making me a strong candidate for any project requiring these skills.
+        My goal is to assist companies and creative people in improving their online presence, specializing in Frontend technologies and AWS Cloud, making me a strong candidate for any project requiring these skills.
       </p>
       <p className='about-paragraph'>
         If you are looking for a Frontend developer with solid technical skills and a user-focused perspective, please feel free to contact me or review my previous projects to see my work.
