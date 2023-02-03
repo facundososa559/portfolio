248432e799b5cece3d9b9f3bea532edb44304ddd
FacundoSosa <facundososa676@gmail.com>
2023-02-03 19:24:25 -0300

    Fix projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,8 +4,8 @@ function Projects() {
   return (
     <section>
       <ul className='center-container text-center text-nowrap p-0 m-0 fs-2 fw-light'>
-          <li className='list-group-item my-3'><a className='text-decoration-none text-black' href="https://openfresh.netlify.app/" target='_blank' rel='noreferrer'>OPENFRESH</a></li>
           <li className='list-group-item my-3'><a className='text-decoration-none text-black' href="https://facundososa.github.io/fernando-de-noronha/" target='_blank' rel='noreferrer'>FERNANDO DE NORONHA</a></li>
+          <li className='list-group-item my-3'><a className='text-decoration-none text-black' href="https://openfresh.netlify.app/" target='_blank' rel='noreferrer'>OPENFRESH</a></li>
           <li className='list-group-item my-3'><a className='text-decoration-none text-black' href="https://facundososa.github.io/libreria-nomadas/?fbclid=IwAR0_juKxUPKoojB0DJ_WaUVWBPEXYeMvuxugJM97OfmgE08fxE-Ll4oPwIc" target='_blank' rel='noreferrer'>LIBRERÍA NÓMADAS</a></li>
       </ul>
     </section>
