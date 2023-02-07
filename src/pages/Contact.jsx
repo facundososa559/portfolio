ca3d13d7e6c9f38a5a793c751c3a7fed8d6520ac
FacundoSosa <facundososa676@gmail.com>
2023-02-07 16:24:31 -0300

    Update contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
         <ul className='contact-container ps-0 m-0'>
           <li className='list-group-item'><p className='m-0'>facundososa676@gmail.com</p></li>
           <li className='list-group-item'><a className='text-decoration-none text-black fs-2' href="https://www.linkedin.com/in/facundososa676/" target='_blank' rel='noreferrer'>LINKEDIN</a></li>
-          <li className='list-group-item'><a className='text-decoration-none text-black fs-2' href="https://www.facebook.com/profile.php?id=100086093425316" target='_blank' rel='noreferrer'>FACEBOOK</a></li>
+          <li className='list-group-item'><a className='text-decoration-none text-black fs-2' href="https://www.instagram.com/facusosa.dev/" target='_blank' rel='noreferrer'>INSTAGRAM</a></li>
         </ul>
     </section>
   )
