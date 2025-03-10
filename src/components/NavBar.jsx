b77a344fdecec98528522927bcd126b7c11c0bcc
Facundo Sosa <facundososa676@gmail.com>
2025-03-09 21:22:26 -0300

    cambio la fuente

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
       <nav className='nav-container text-end'>
         <ul className='navbar container--flex'>
           <li><a href="#projects" className='navbar__link'>PROYECTOS</a></li>
-          <li><a href="#faq" className='navbar__link'>PREGUNTAS FRECUENTES</a></li>
+          <li><a href="#faq" className='navbar__link'>FAQ</a></li>
           <li><a href="#contact">CONTACTO</a></li>
         </ul>
       </nav>
