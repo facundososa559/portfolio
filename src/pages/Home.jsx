98372d35438adbf6b62733978a577f0e6d0e53b0
Facundo Sosa <facundososa676@gmail.com>
2025-03-16 09:31:51 -0300

    cambio headline

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,14 +22,14 @@ function Home() {
                 <h1 className='text--small extra-light letter-spacing hero__subheading'>FACU DEL VALLE</h1>
               </AnimatedElement>
               <AnimatedElement delay={0.2}>
-                <h1 className='heading semi-bold'>Profesionalismo, seriedad y diferenciación para tu negocio.</h1>
+                <h1 className='heading hero-heading semi-bold'>Diseño personalizado de redes sociales y sitios web.</h1>
               </AnimatedElement>
               <AnimatedElement delay={0.3}>
-                <p>Diseño, desarrollo y optimizo sitios web para que tus clientes reconozcan el valor de tus productos o servicios, en un lugar que brinda autoridad y presencia.</p>
+                <p>Si te interesa subir el nivel de tus redes sociales y construir una arquitectura digital con profesionalidad y diferenciación, puedo ayudarte.</p>
               </AnimatedElement>
               
               <AnimatedElement delay={0.4}>
-                <p>Te invito a ver mis diseños, si te interesan, te envío los datos de los diferentes packs de servicios que tengo, para que elijas cuál se adapta mejor a tu negocio.</p>
+                <p>Haciendo click en las opciones, te envío los datos de los diferentes packs de servicios que tengo para que elijas cuál se adapta mejor a tu negocio.</p>
               </AnimatedElement>
               <AnimatedElement delay={0.5}>
                 <CallToAction 
